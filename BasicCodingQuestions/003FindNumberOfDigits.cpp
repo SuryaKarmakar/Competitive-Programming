e647f3a44c187bbc0d1ea29c12f9430f8dd5af15
SuryaKarmakar <surjokarmakarssj7@gmail.com>
2021-10-12 13:36:03 +0530

    write logic of calculate total number of digits

--- BasicCodingQuestions/003FindNumberOfDigits.cpp
+++ BasicCodingQuestions/003FindNumberOfDigits.cpp
@@ -1,4 +1,13 @@
-/* Write a program to find Number of digits in an integer */
+/* Write a program to find Number of digits in an integer
+[+]Logic : After taking the input from the user, run while loop until the user given number becomes 0 or less than 0. Inside the loop for each iteration increase the 
+counter variable by one and divide the user given number by 10 and store it in the same variable again.
+Once the loop is over we have to print counter variable count containing the total number of digits.
+[+]Example : if user enter 2240 then :
+2240 / 10 remaining 224 => now count 1,
+224 / 10 remaining 22 => now count 2,
+22 / 10 remaining  2 => now count 3,
+2 / 10 remaining  0 => now count 4 
+*/
 
 #include<iostream>
 using namespace std;
