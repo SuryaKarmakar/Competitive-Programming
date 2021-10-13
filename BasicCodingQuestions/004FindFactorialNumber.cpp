999db057b01de22e74cbae1a9c2958280acc1168
SuryaKarmakar <surjokarmakarssj7@gmail.com>
2021-10-13 19:33:52 +0530

    find factorial of a number

---
+++ BasicCodingQuestions/004FindFactorialNumber.cpp
@@ -0,0 +1,23 @@
+/* Write a program to find Factorial of a number */
+
+#include<iostream>
+using namespace std;
+
+int findFactorialNumber(int x){
+    int fact = 1;
+    int i;
+
+    for(i = 1 ; i <= x ; i++)
+        fact = fact * i;
+    return fact; 
+}
+
+int main(){
+    int x, result;
+    cout << "Enter A Number : ";
+    cin >> x;
+
+    result = findFactorialNumber(x);
+    cout << "Factorial Of This " << x << " Number Is : " << result;
+    return 0;
+}
